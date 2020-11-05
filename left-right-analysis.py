df292f46e40dbc3cfec1dd6b7f65614065e36463
frimpz <boadu203@gmail.com>
2020-11-05 01:51:45 -0600

    added gender file

--- left-right-analysis.py
+++ left-right-analysis.py
@@ -8,6 +8,7 @@ from functools import reduce
 from plots import create_boxplots
 import matplotlib.pyplot as plt
 from utils import read_csv
+import scipy.stats as st
 
 
 def score_strategies(search_strategy):
@@ -96,51 +97,126 @@ result.loc[result['Right_categ'].str.startswith("AGG"), 'Right_categ'] = "Aggres
 
 # result['left_select'] = result.apply(orientation, pos="left",  axis=1)
 # result['right_select'] = result.apply(orientation, pos="right",  axis=1)
-result['orientation'] = result.apply(orientation, pos="both",  axis=1)
+result['Orientation'] = result.apply(orientation, pos="both",  axis=1)
 
+monkey_df = result[['Monkey', 'Left_categ', 'Right_categ', 'Orientation']]
+monkeys = list(set((monkey_df.Monkey)))
 
-gender_df = result[['gender', 'Left_categ', 'Right_categ', 'orientation']]
+gender_df = result[['gender', 'Left_categ', 'Right_categ', 'Orientation']]
 genders = list(set((gender_df.gender)))
+
+print(monkey_df)
+print(gender_df)
+exit()
+"""
+    Method creates  the excel file and box plot
+    :param excel_rows: all columns that appear in excel file
+    monkey : ['Monkey', 'Left-Category', 'Right-Category', 'Category', 'Count', 'Probability', 'lower CI', 'upper CI', 'P-Value']
+    gender:  ['gender', 'Left-Category', 'Right-Category', 'Category', 'Count', 'Probability', 'lower CI', 'upper CI', 'P-Value']
+    :param item_name: Monkey/gender
+    :param items: Monkey list/ gender list
+    :param file_name: filename of excel
+    :param df_cols : pandas column to limit 
+    :param groupby_cols : pandas group by column
+    :param bp_group : boxplot groupby
+    :return : None
+    """
+
+
+def distribution(excel_rows, item_name,  items, file_name, df_cols, groupby_cols, bp_group):
+    # Using 95% confidence interval
+    # (1-0.95)/2
+    Z_score = abs(st.norm.ppf(0.025))
+    alpha = 1-0.95
+    data_files = {}
+
+    # create dataframe
+    for item in items:
+        if item_name == "Monkey":
+            df = (monkey_df[(monkey_df.Monkey == item)])
+        elif item_name == "gender":
+            df = (gender_df[(gender_df.gender == item)])
+        z = BayesianEstimator(model, df)
+        cat_cpd = z.estimate_cpd('Category', prior_type="bdeu", equivalent_sample_size=6)  # .to_factor()
+        for left in categories:
+            for right in categories:
+                for cat in categories:
+                    try:
+                        count = z.state_counts('Category')[left][right][cat]
+                        prob = cat_cpd.get_value(
+                            **{'Left_categ': left, 'Right_categ': right, 'Category': cat})
+
+                        # p_hat and q_hat set to conservative since we have no previous data #0.5 for each
+                        # Since its probability I clip to 0
+                        lower_ci = max(prob - Z_score * math.sqrt((0.5*0.5)/df.shape[0]), 0)
+                        upper_ci = prob + Z_score * math.sqrt((0.5*0.5)/df.shape[0])
+                        if not isNaN(prob) and prob > 0:
+                            excel_rows.append([item, left, right, cat, count, prob, lower_ci, upper_ci, alpha])
+                        else:
+                            pass
+                            # excel_rows.append([item, left, right, cat, count, prob, 0, 0, 0])
+                    except KeyError:
+                            pass
+                            # excel_rows.append([item, left, right, cat, count, 0, 0 , 0, 0])
+
+    prob_df = pd.DataFrame.from_records(excel_rows[1:], columns=excel_rows[0])
+    gen_df = prob_df[df_cols].groupby(groupby_cols)['Count'].agg(['sum'])# .reset_index()
+
+    ax, bp = gen_df.boxplot(rot=90, fontsize=12, figsize=(8, 10), column=['sum'], by=bp_group, return_type="both")[0]
+    plt.title(item_name.capitalize()+ " Box plot grouped by : " + str(bp_group))
+    plt.suptitle('')
+    plt.ylabel("sum")
+
+    # group = ['Left-Category', 'Category']
+    # ax, bp = gen_df.boxplot(rot=90, fontsize=12, figsize=(24, 12), column=['sum'], by=group, return_type="both")[0]
+    # plt.title("Box plot grouped by : " + str(group))
+    # plt.suptitle('')
+    # plt.ylabel("sum")
+    #
+    #
+    # group = ['Right-Category', 'Category']
+    # ax, bp = gen_df.boxplot(rot=90, fontsize=12, figsize=(24, 12), column=['sum'], by=group, return_type="both")[0]
+    # plt.title("Box plot grouped by : " + str(group))
+    # plt.suptitle('')
+    # plt.ylabel("sum")
+
+    writer = pd.ExcelWriter(file_name+".xlsx")
+    prob_df.to_excel(writer, sheet_name='Distribution')
+    prob_df.sort_values('Probability', ascending=False).drop_duplicates([item_name]).to_excel(writer, sheet_name='prefference')
+    writer.save()
+
+    plt.savefig(file_name+".png", dpi=100)
+    plt.show()
+    plt.clf()
+
+
+# for monkeys
 categories = ['Affiliative', 'Aggressive', 'Cynomolgus', 'Fruit',  'Lab', 'Nature']
-model = BayesianModel([('Left_categ', 'orientation'), ('Right_categ', 'orientation')])
+model = BayesianModel([('Left_categ', 'Category'), ('Right_categ', 'Category')])
+
+excel_rows = [['Monkey', 'Left-Category', 'Right-Category', 'Category', 'Count', 'Probability', 'lower CI', 'upper CI', 'alpha']]
+items = monkeys
+file_name = "results/monkey"
+
+df_cols = ['Left-Category', 'Right-Category', 'Category', 'Count']
+groupby_cols = ['Left-Category', 'Right-Category', 'Category']
+bp_group = ['Category']
+item_name = 'Monkey'
+
+distribution(excel_rows, item_name,  items, file_name, df_cols, groupby_cols, bp_group)
 
 
-data_files = {}
-temp = [['gender', 'Left-Category', 'Right-Category', 'orientation', 'Count', 'Probability']]
-for gender in genders:
-    df = (gender_df[(gender_df.gender == gender)])
-    z = BayesianEstimator(model, df)
+# for gender
+categories = ['Affiliative', 'Aggressive', 'Cynomolgus', 'Fruit',  'Lab', 'Nature']
+model = BayesianModel([('Left_categ', 'Category'), ('Right_categ', 'Category')])
 
-    cat_cpd = z.estimate_cpd('orientation', prior_type="bdeu", equivalent_sample_size=6)  # .to_factor()
-    for left in categories:
-        for right in categories:
-            for cat in ['left', 'right']:
-                try:
-                    count = z.state_counts('orientation')[left][right][cat]
-                    prob = cat_cpd.get_value(
-                        **{'Left_categ': left, 'Right_categ': right, 'orientation': cat})
-                    if not isNaN(prob) and prob > 0:
-                        temp.append([gender, left, right, cat, count, prob])
-                    else:
-                        temp.append([gender, left, right, cat, count, prob])
-                except KeyError:
-                    pass
-                        #temp.append([gender, left, right, cat, count, 0])
-#
-prob_df = pd.DataFrame.from_records(temp[1:], columns=temp[0])
+excel_rows = [['gender', 'Left-Category', 'Right-Category', 'Category', 'Count', 'Probability', 'lower CI', 'upper CI', 'alpha']]
+items = genders
+file_name = "results/gender"
 
-print(prob_df)
+df_cols = ['gender', 'Left-Category', 'Right-Category', 'Category', 'Count']
+groupby_cols = ['gender', 'Left-Category', 'Right-Category', 'Category']
+bp_group = ['gender', 'Category']
+item_name = 'gender'
 
-gen_df = prob_df[['gender', 'Left-Category', 'Right-Category', 'orientation', 'Count']].\
-    groupby(['gender', 'Left-Category', 'Right-Category', 'orientation'])['Count'].agg(['sum'])# .reset_index()
-# boxplot = gen_df.boxplot(rot=45, fontsize=12, figsize=(8, 10))
-print(gen_df)
-#
-group = ['gender', 'orientation']
-ax, bp = gen_df.boxplot(rot=90, fontsize=12, figsize=(12, 10), column=['sum'], by=group, return_type="both")[0]
-plt.title("Box plot grouped by : " + str(group))
-plt.suptitle('')
-plt.ylabel("sum")
-
-
-plt.show()
+distribution(excel_rows, item_name,  items, file_name, df_cols, groupby_cols, bp_group)
